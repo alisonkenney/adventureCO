37d02e29169b26d8d02f98dd0b2fa6ee4bc5fa67
Alison Kenney <alisonkenney1@gmail.com>
2016-07-21 09:08:53 -0600

    all routes working - still need some styling

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ class AdventureCO < Sinatra::Base
 	
 	 set :method_override, true
 
-	# General route actions
-	
+	# General route actions	
 	#get home
 	get '/' do
 		erb :home
@@ -14,39 +13,55 @@ class AdventureCO < Sinatra::Base
 		erb :about
 	end
 
-	#get index
+	#index
 	get '/adventures' do
 		@category = params[:category] || "all"
 		if @category == "all"
-			@adventures = Adventure.all	
+			@adventures = Adventure.all.order(:name)	
 		else
-			@adventures = Adventure.where(category: @category)
+			@adventures = Adventure.where(category: @category).order(:name)
 		end	
     	erb(:"adventure/index")
 	end	
 
-	#get new
-	get '/adventures/new' do
-		@adventure = Adventure.new
-		erb(:"adventure/new")
-	end
+    #create
+    post '/adventures' do
+      adventure = params[:adventure]
+      new_adventure = Adventure.create(adventure)
+      redirect to("/adventures?category=#{adventure[:category]}")
+    end
 
-	#create new
-  	post '/adventures' do
-    	@adventure = Adventure.new(params[:adventure])
-    	if @adventure.save
-      		redirect("/adventure/#{@adventure.id}")
-    	else
-      		erb(:"adventure/new")
-    	end
-  	end
-
-  	 #show
-  	get '/adventure/:id' do
+  	#show
+  	get '/adventures/:id' do
     	@adventure = Adventure.find(params[:id])
     	erb(:"adventure/show")
   	end
 
+  	#edit
+  	get '/adventures/:id/edit' do
+    	@adventure = Adventure.find(params[:id])
+    	erb(:"adventure/edit")
+  	end
 
+  	# update selected
+  	put '/adventures/:id' do
+    	@adventure = Adventure.find(params[:id])
+    	if @adventure.update_attributes(params[:adventure])
+    		 @adventure.save
+      	 redirect("/adventures/#{@adventure.id}")
+    	else
+      		erb(:"adventure/edit")
+    	end
+  	end
+
+  # delete
+  	delete '/adventures/:id' do
+    	@adventure = Adventure.find(params[:id])
+    	if @adventure.destroy
+      		redirect('/')
+    	else
+      		redirect("/adventures/#{@adventure.id}")
+    	end
+  	end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,26 +3,62 @@ require './models/adventure'
 Adventure.create({
     category: 'Sporting',
     name: 'Denver Broncos Game', 
-    description: 'Cheer on the Denver Broncos as they try for another Super Bowl win!',
-    photo_url: "http://images.performgroup.com/di/library/omnisport/af/a9/mile-high-stadium-091415-usnews-getty-ftr_1go9fllt0jux21f0qoongpyrz9.jpg?t=-840085121", 
-    price: "150",
+    description: 'The <a href="http://www.denverbroncos.com/" target="_blank">Denver Broncos</a> are an American football team based in Denver, Colorado. The Broncos compete in the National Football League (NFL) as a member club of the leagues American Football Conference (AFC) West division.',
+    photo_url: "/images/broncos.jpg", 
+    price: "100+",
     location: "1701 Bryant St, Denver, CO 80204"
   })
 
 Adventure.create({
     category: 'Sporting',
     name: 'Colorado Rockies Game', 
-    description: 'Cheer on the Colorado Rockies!',
-    photo_url: "http://guardianlv.com/wp-content/uploads/2014/06/Coors-Field-Welcome-Relief-Following-Nine-Game-Road-Trip-Rockies-Rundown-650x328.jpg", 
-    price: "10",
+    description: 'The <a href="http://colorado.rockies.mlb.com/index.jsp?c_id=col" target="_blank">Colorado Rockies</a> are an American professional baseball team based in Denver, Colorado. Their home venue is Coors Field. They are currently members of Major League Baseball (MLB)s National League (NL) West division.',
+    photo_url: "/images/rockies.jpg", 
+    price: "$0 - $50",
     location: "2001 Blake St, Denver, CO 80205"
+  })
+
+Adventure.create({
+    category: 'Sporting',
+    name: 'Denver Nuggets Game', 
+    description: 'The <a href="http://www.nba.com/nuggets/draft/" target="_blank">Denver Nuggets</a> are an American professional basketball team based in Denver, Colorado. The Nuggets compete in the National Basketball Association (NBA) as a member club of the leagues Western Conference Northwest Division.',
+    photo_url: "/images/nuggets.jpg", 
+    price: "$0 - $50",
+    location: "1000 Chopper Cir, Denver, CO 80202"
+  })
+
+Adventure.create({
+    category: 'Sporting',
+    name: 'Colorado Avalanche Game', 
+    description: 'The <a href="http://www.nba.com/nuggets/draft/" target="_blank">Colorado Avalanche</a> are a professional ice hockey team based in Denver, Colorado. They are members of the Central Division of the Western Conference of the National Hockey League (NHL).',
+    photo_url: "/images/avalanche.jpeg", 
+    price: "$0 - $50",
+    location: "1000 Chopper Cir, Denver, CO 80202"
+  })
+
+Adventure.create({
+    category: 'Sporting',
+    name: 'Colorado Mammoth Game', 
+    description: 'The <a href="http://www.coloradomammoth.com/" target="_blank">Colorado Mammoth</a> are a member of the National Lacrosse League. They have played at Pepsi Center in Denver, Colorado, United States, since the 2003 season. In 2004 they became the leagues attendance leader, closely beating the Toronto Rock.',
+    photo_url: "/images/mammoth.png", 
+    price: "$0 - $50",
+    location: "1000 Chopper Cir, Denver, CO 80202"
+  })
+
+Adventure.create({
+    category: 'Social',
+    name: 'Denver Beer CO', 
+    description: 'Brewery-based taproom with an industrial vibe, dog-friendly outdoor beer garden & food trucks.',
+    photo_url: "/images/dbc.jpg", 
+    price: "$0 - $50",
+    location: "1000 Chopper Cir, Denver, CO 80202"
   })
 
 Adventure.create({
     category: 'Adrenaline',
     name: 'Mile-Hi Skydiving', 
-    description: 'Sky-dive facility for all experience levels, from tandem jumps to solo, including training classes.!',
+    description: 'Sky-dive facility for all experience levels, from tandem jumps to solo, including training classes!',
     photo_url: "http://coloradoairtours.com/images/skydive-colorado2.jpg", 
-    price: "100",
+    price: "100+",
     location: "229 Airport Rd #34G, Longmont, CO 80503"
   })
